85c7b3b1048d536aba945ef4171c52f2c3694267
Thomas Roux <roux.t@sfeir.com>
2020-03-09 23:02:47 +0100

    use hyper instead of iterm2 for terminal

--- cask/install.sh
+++ cask/install.sh
@@ -21,7 +21,7 @@ brew cask install dashlane 2> /dev/null
 brew cask install slack 2> /dev/null
 brew cask install spectacle 2> /dev/null
 brew cask install docker 2> /dev/null
-brew cask install iterm2 2> /dev/null
+brew cask install hyper 2> /dev/null
 brew cask install boostnote 2> /dev/null
 
 # Fonts
